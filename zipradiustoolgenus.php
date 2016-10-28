7027cee249fe25d932ae00f70cd6dbfa2c8c6bca
Pete Wingard <petewingard@gmail.com>
2016-10-28 15:55:33 -0400

    changed file name

--- zipradiustoolgenus.php
+++
@@ -1,8 +0,0 @@
-<?php
-//require_once '/home/pwingard/hidden/connect2db.php';//gets my  db connection as $conn
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
